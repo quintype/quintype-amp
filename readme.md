eed32e45b80d6c05e3b8c0e1232fc000c2b83785
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-02-04 21:57:54 +0530

    running webpack in --watch mode, added nodemon | moved express as dev dependency

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 # ⚡ Quintype Amp ⚡
+
+## To run:
+```npm i && npm run dev```
--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const str = require('./dist/bundle')
-
-// TEMPERORY SERVER just to see some output.. This will not be part of the library obviously
-
-app.all("*", (req, res) => res.status(200).send(str.default))
-
-app.listen(3000, () => console.log("App listening on port 3000"))
