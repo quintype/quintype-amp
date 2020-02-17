d26edf76253eba816e4be9198b0a927aae32c76b
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-02-17 13:03:02 +0530

    removed unnecessary code

--- src/components/atoms/Carousel/carousel.stories.js
+++ src/components/atoms/Carousel/carousel.stories.js
@@ -11,9 +11,9 @@ const AmpDecorator = (storyFn) => {
 
   return (
     <div>
-    <Helmet >
-    <script src="https://cdn.ampproject.org/v0.js" async=""></script>
-    <script async={undefined} custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.2.js"></script>
+    <Helmet>
+      <script src="https://cdn.ampproject.org/v0.js" async=""></script>
+      <script async={undefined} custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.2.js"></script>
       <style amp-custom="">{styles.toString()}</style>
     </Helmet>
     { storyFn() }
@@ -22,22 +22,22 @@ const AmpDecorator = (storyFn) => {
   };
 
   storiesOf('Carousel', module)
-  .addDecorator(AmpDecorator)
-  .add('with images', () => (
-    <Carousel height="300" width="500"
-  layout="responsive"
-  type="slides">
-  <amp-img src="https://images.unsplash.com/photo-1558980395-be8a5fcb4251?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-    width="400"
-    height="300"
-    alt="a sample image"></amp-img>
-  <amp-img src="https://images.unsplash.com/photo-1581793111741-04b7034d1cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-    width="400"
-    height="300"
-    alt="another sample image"></amp-img>
-  <amp-img src="https://images.unsplash.com/photo-1581911705179-8e5221e6be29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-    width="400"
-    height="300"
-    alt="another sample image"></amp-img>
-</Carousel>
-  ));
+    .addDecorator(AmpDecorator)
+    .add('with images', () => (
+      <Carousel height="300" width="500"
+        layout="responsive"
+        type="slides">
+        <amp-img src="https://images.unsplash.com/photo-1558980395-be8a5fcb4251?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          width="400"
+          height="300"
+          alt="a sample image"></amp-img>
+        <amp-img src="https://images.unsplash.com/photo-1581793111741-04b7034d1cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          width="400"
+          height="300"
+          alt="another sample image"></amp-img>
+        <amp-img src="https://images.unsplash.com/photo-1581911705179-8e5221e6be29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          width="400"
+          height="300"
+          alt="another sample image"></amp-img>
+      </Carousel>
+    ));
