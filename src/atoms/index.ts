617c1a2d2c7f03bcb195a86df8563cd6933e5374
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-02-21 10:44:00 +0530

    Remove exports for deleted components

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,8 +1,5 @@
 import Carousel from "./carousel";
-import FitText from "./fit-text";
 import Image from "./image";
-import Title from "./title";
-import Youtube from "./youtube";
 import Layout from "./layout";
 
-export { Carousel, FitText, Image, Title, Youtube, Layout };
+export { Carousel, Image, Layout };
