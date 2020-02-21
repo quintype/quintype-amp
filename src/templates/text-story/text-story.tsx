c251b0bd80d357ec44e46ca8c2ff8f072f19acc1
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-02-21 10:18:57 +0530

    Adds a text story template

---
+++ src/templates/text-story/text-story.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout } from "../../atoms";
+
+const TextStory = () => {
+  return (
+    <Layout>
+      <h1>This is a text story</h1>
+    </Layout>
+  );
+};
+
+export { TextStory };
