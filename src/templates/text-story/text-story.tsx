8e28c7703889817f90692eaaaa99c07243d5fde8
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-02-24 08:26:41 +0530

    Adds Story context, types and fixture (#15)

--- src/templates/text-story/text-story.tsx
+++ src/templates/text-story/text-story.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Layout } from "../../atoms";
+import { textStory } from "../../__fixtures__/story.fixture";
 
-const TextStory = () => {
+const TextStory = ({ story = textStory }: any) => {
   return (
-    <Layout>
+    <Layout story={story}>
       <h1>This is a text story</h1>
     </Layout>
   );
