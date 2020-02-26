3c4db799068ec6e16d1268dd3a15104f028da0df
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-02-26 15:44:13 +0530

    Add footer styles

--- src/atoms/footer/footer.tsx
+++ src/atoms/footer/footer.tsx
@@ -2,8 +2,22 @@ import React, { Fragment } from "react";
 import styled from "styled-components";
 import { FooterTypes } from "./types";
 
-const StyledFooter = styled.footer``;
+const StyledFooter = styled.footer`
+  display: flex;
+  padding: 8px 4px;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  min-height: 50px;
+  font-family: ${(props) => props.theme.font.family.secondary};
+  background-color: ${(props) => props.theme.color.primaryColor};
+`;
 
+const PoweredBy = styled.a`
+  text-decoration: none;
+  color: ${(props) => props.theme.color.footerTextColor};
+  font-size: ${(props) => props.theme.font.size.xxs};
+`;
 const Footer = ({ text, children }: FooterTypes) => {
   return (
     <StyledFooter>
@@ -11,10 +25,10 @@ const Footer = ({ text, children }: FooterTypes) => {
         children
       ) : (
         <Fragment>
-          <p>{text}</p>
-          <a href="https://www.quintype.com/" rel="noreferrer noopener" target="_blank">
+          {text && <p>{text}</p>}
+          <PoweredBy href="https://www.quintype.com/" rel="noreferrer noopener" target="_blank">
             Powered by Quintype
-          </a>
+          </PoweredBy>
         </Fragment>
       )}
     </StyledFooter>
