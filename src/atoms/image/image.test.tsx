66f8f9c7f6da1e282762e7c62a925ff3fb87ae06
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-02-26 10:23:38 +0530

    removed unnecessary import

--- src/atoms/image/image.test.tsx
+++ src/atoms/image/image.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import { BaseImage } from "./image";
 import get from "lodash/get";
 import { textStory } from "../../__fixtures__/story.fixture";
