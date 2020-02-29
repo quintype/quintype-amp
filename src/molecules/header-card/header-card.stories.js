c9c2285929ee689b218a64a5d9d9ba35852cf5ab
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-02-29 18:52:48 +0530

    Fix header card story not rendering

--- src/molecules/header-card/header-card.stories.js
+++ src/molecules/header-card/header-card.stories.js
@@ -3,7 +3,12 @@ import { storiesOf } from "@storybook/react";
 import { HeaderCard } from "./header-card";
 import { Layout } from "../../atoms";
 import { textStory } from "../../__fixtures__/story.fixture";
+import { config } from "../../__fixtures__/config.fixture";
 
 storiesOf("Header Card", module)
-  .addDecorator((story) => <Layout story={textStory}>{story()}</Layout>)
+  .addDecorator((story) => (
+    <Layout story={textStory} config={config}>
+      {story()}
+    </Layout>
+  ))
   .add("default", () => <HeaderCard />);
