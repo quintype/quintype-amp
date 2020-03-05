bc582be3686f3d05b78044c102a32f91a0f21a06
Amogh S <amogh1773@gmail.com>
2020-03-05 10:11:25 +0530

    Create a HOC for passing story and config context (#22)

--- src/molecules/header-card/header-card.tsx
+++ src/molecules/header-card/header-card.tsx
@@ -1,10 +1,13 @@
 import React, { useContext } from "react";
-import { StoryContext } from "../../context/storyContext";
+import { StoryContext } from "../../context/story-context";
 import { Story } from "../../types/story";
+import { withStoryAndConfig } from "../../context/with-story-config";
 
-const HeaderCard = () => {
+const HeaderCardBase = () => {
   const story = useContext(StoryContext) as Story;
   return <h1>{story.headline}</h1>;
 };
+
+const HeaderCard = withStoryAndConfig(HeaderCardBase);
 
 export { HeaderCard };
