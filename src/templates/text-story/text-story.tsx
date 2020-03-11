9a630fd134e41fc710f30ce72315263608ac7623
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-03-11 13:04:19 +0530

    added navbar to textStory

--- src/templates/text-story/text-story.tsx
+++ src/templates/text-story/text-story.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Layout } from "../../atoms";
-import { HeroImage } from "../../molecules";
+import { HeroImage, Navbar } from "../../molecules";
 
 const TextStory = ({ story, config }) => {
   return (
     <Layout story={story} config={config}>
+      <Navbar />
       <HeroImage />
       <h1>This is a text story</h1>
     </Layout>
