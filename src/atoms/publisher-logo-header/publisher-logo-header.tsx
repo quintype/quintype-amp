a3f03d4b1969e9cf1fc6949fbfcb4e43347015f7
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-03-11 13:01:32 +0530

    styling change in publisher-logo

--- src/atoms/publisher-logo-header/publisher-logo-header.tsx
+++ src/atoms/publisher-logo-header/publisher-logo-header.tsx
@@ -2,17 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import { PublisherLogoHeaderTypes } from "./types";
 
-const StyledWrapper = styled.span`
+const StyledImg = styled.img`
   width: 200px;
-  height: 50px;
 `;
 
 export const PublisherLogoHeader = ({ logoSrc }: PublisherLogoHeaderTypes) => {
   return (
-    <StyledWrapper>
-      <a href="/">
-        <img src={logoSrc} />
-      </a>
-    </StyledWrapper>
+    <a href="/">
+      <StyledImg src={logoSrc} />
+    </a>
   );
 };
