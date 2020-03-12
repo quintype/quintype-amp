561741c4889292de84308c68de06c383e2674b7e
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-03-12 13:05:53 +0530

    Expose templates as a separate key

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { renderToString, ampifyStory } from "./helpers";
+import * as templates from "./templates";
 import * as atoms from "./atoms";
 import * as molecules from "./molecules";
 
-const Amp = { ...atoms, ...molecules };
-export { Amp, renderToString, ampifyStory };
+const Templates = { ...templates };
+const AMP = { ...atoms, ...molecules };
+export { AMP, Templates, renderToString, ampifyStory };
