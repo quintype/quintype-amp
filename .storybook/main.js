e6a15ec860dba6b6ab49e6b5b65d73670c0c5ab6
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-03-12 13:06:15 +0530

    Use default typescript addon for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,4 @@
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve("ts-loader")
-        }
-      ]
-    });
-    config.resolve.extensions.push(".ts", ".tsx");
-    return config;
-  }
+  addons: ["@storybook/preset-typescript"]
 };
