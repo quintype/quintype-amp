a7697fa21d3f5bf2faf3dff282c7df690ee07aaa
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-03-13 10:58:12 +0530

    Adds section atom

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -4,5 +4,6 @@ import Layout from "./layout";
 import Facebook from "./facebook";
 import Twitter from "./twitter";
 import Footer from "./footer";
+import { Section } from "./section";
 
-export { Carousel, Image, Layout, Facebook, Twitter, Footer };
+export { Carousel, Image, Layout, Facebook, Twitter, Footer, Section };
---
+++ src/atoms/section/section.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { SectionProps } from "./types";
+import styled from "styled-components";
+
+const StyledSection = styled.h5`
+  font-family: ${(props) => props.theme.font.family.primary};
+  font-size: ${(props) => props.theme.font.size.xs};
+  letter-spacing: 1px;
+  margin: 5px 15px;
+  font-weight: bold;
+`;
+
+const Section = ({ section }: SectionProps) => {
+  const sectionName =
+    section["display-name"] && section["display-name"].length > 1 ? section["display-name"] : section.name;
+
+  return <StyledSection>{sectionName}</StyledSection>;
+};
+
+export { Section };
