376a6c636b8d38e32fc5ed3395049a90070517d2
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-03-13 09:34:15 +0530

    Fix templates import

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import { renderToString, ampifyStory } from "./helpers";
-import * as templates from "./templates";
+import * as Templates from "./templates";
 import * as atoms from "./atoms";
 import * as molecules from "./molecules";
 
-const Templates = { ...templates };
 const AMP = { ...atoms, ...molecules };
 export { AMP, Templates, renderToString, ampifyStory };
