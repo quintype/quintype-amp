0696f5808609a4af41a0338a7cac9f29eea5cfa9
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-03-17 07:42:43 +0530

    Add components to header card

--- src/molecules/header-card/header-card.tsx
+++ src/molecules/header-card/header-card.tsx
@@ -1,9 +1,33 @@
 import React from "react";
 import { withStoryAndConfig } from "../../context";
 import { HeaderCardTypes } from "./types";
+import { HeroImage } from "../index";
+import { Spacer, Section, Author } from "../../atoms";
+import styled from "styled-components";
+
+const Headline = styled.h1`
+	font-family: ${(props) => props.theme.font.family.primary};
+	font-size: ${(props) => props.theme.font.size.l};
+	color: ${(props) => props.theme.color.black}
+	line-height: ${(props) => props.theme.font.lineHeight.level2};
+	margin: 0;
+	font-weight: bold;
+}
+`;
 
 const HeaderCardBase = ({ story }: HeaderCardTypes) => {
-  return <h1>{story.headline}</h1>;
+  return (
+    <div>
+      <HeroImage story={story} />
+      <Spacer token="xs" />
+      <Section section={story.sections[0]} />
+      <Spacer token="xs" />
+      <Headline>{story.headline}</Headline>
+      <Spacer token="xs" />
+      <Author authors={story.authors} prepend="By" />
+      <Spacer token="xs" />
+    </div>
+  );
 };
 
 const HeaderCard = withStoryAndConfig(HeaderCardBase);
