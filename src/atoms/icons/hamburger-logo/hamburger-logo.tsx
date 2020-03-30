315a3f11c90204446e979ef6a79fad16755ca628
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-03-30 10:58:59 +0530

    currentColor

--- src/atoms/icons/hamburger-logo/hamburger-logo.tsx
+++ src/atoms/icons/hamburger-logo/hamburger-logo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const HamburgerLogo = ({ width = "54px", height = "54px", color = "black" }) => {
+export const HamburgerLogo = ({ width = "54px", height = "54px", color = "currentColor" }) => {
   return (
     <svg width={width} height={height} viewBox="0 -53 384 384" xmlns="http://www.w3.org/2000/svg">
       <path
