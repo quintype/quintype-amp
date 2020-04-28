af014baf45b437eaba34f18c8f2b6859d9d46f0c
Ravigopal Kesari <kesariravigopal@gmail.com>
2020-04-28 08:10:43 +0530

    Adds Instagram element

---
+++ src/atoms/instagram/instagram.tsx
@@ -0,0 +1,28 @@
+import React, { Fragment } from "react";
+import { InstagramTypes } from "./types";
+import { Helmet } from "react-helmet";
+
+export const Instagram = (props: InstagramTypes) => {
+  const { width, height, layout } = props;
+  const setDefaultLayout = !width || !height || !layout;
+  const componentProps: InstagramTypes = setDefaultLayout
+    ? {
+        ...props,
+        width: "16",
+        height: "9",
+        layout: "responsive"
+      }
+    : props;
+  return (
+    <Fragment>
+      <Helmet>
+        <script
+          async={undefined}
+          custom-element="amp-instagram"
+          src="https://cdn.ampproject.org/v0/amp-instagram-0.1.js"
+        />
+      </Helmet>
+      <amp-instagram {...componentProps} />
+    </Fragment>
+  );
+};
---
+++ src/atoms/instagram/types.ts
@@ -0,0 +1,6 @@
+import { Common } from "../common-types";
+
+export interface InstagramTypes extends Common {
+  "data-shortcode": string;
+  "data-captioned"?: boolean;
+}
