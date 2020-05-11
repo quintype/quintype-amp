1475a7db0104960b1743e274536a0d362b0682a9
Amogh S <amogh1773@gmail.com>
2020-05-11 10:24:47 +0530

    Related stories fix (#120)
    * removed responsive behaviour from related stories to match platform amp
    
    * css fixes for related-story
    
    * bump to 1.0.9
    
    Co-authored-by: Amogh Sahasrabhojanee <amogh@quintype.com>

--- src/molecules/related-stories/related-stories.tsx
+++ src/molecules/related-stories/related-stories.tsx
@@ -1,31 +1,22 @@
 import React, { Fragment } from "react";
 import styled from "styled-components";
 import { RelatedStoryCard } from "../related-story-card";
-import { media } from "../../utils/media";
 import { RelatedStoriesTypes } from "./types";
 import { withConfig } from "../../context";
 import { base64FallbackImage } from "../../helpers/image-helpers";
+import { Spacer } from "../../atoms";
 import get from "lodash.get";
 
-const RelatedStoryCards = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  ${media.laptop`
-    flex-direction: row;
-	`}
-`;
 export const Heading = styled.h2`
-  font-size: ${(props) => props.theme.font.size.m};
-  border-bottom: 1px solid ${(props) => props.theme.color.mono4};
-  padding-bottom: 3px;
+  font-size: ${(props) => props.theme.font.size.l};
 `;
 
 export const RelatedStoriesBase = ({ stories, config, heading, aspectRatio = [16, 9] }: RelatedStoriesTypes) => {
   return stories && stories.length ? (
     <Fragment>
-      <Heading>{heading || "Related Stories"}</Heading>
-      <RelatedStoryCards>
+      <Spacer token="m" />
+      <Heading>{heading || "Also Read"}</Heading>
+      <div>
         {stories.map((story) => (
           <RelatedStoryCard
             key={story.id}
@@ -34,7 +25,7 @@ export const RelatedStoriesBase = ({ stories, config, heading, aspectRatio = [16
             aspectRatio={aspectRatio}
           />
         ))}
-      </RelatedStoryCards>
+      </div>
     </Fragment>
   ) : null;
 };
