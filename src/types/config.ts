9fdf5e8a5e367492c42f123d20917528a7b302be
Shraddha Kesari <shraddha.kesari@gmail.com>
2020-05-12 17:08:00 +0530

    Adds footer text and publisher settings to the config (#123)
    * Adds footer text and publisher settings to the config
    
    * Added check for publisher settings

--- src/types/config.ts
+++ src/types/config.ts
@@ -9,6 +9,7 @@ export interface PublisherConfig {
   "text-direction": "ltr" | "rtl";
   "publisher-name": string;
   "publisher-id": number;
+  "publisher-settings"?: object;
 }
 
 interface Colors {
@@ -60,26 +61,26 @@ interface DoubleClickAdTypes {
 }
 
 interface MenuItemTypes {
-  "tag-name": string | null,
-  "entity-properties": string | null,
-  "collection-id": number | null,
-  "entity-slug": string | null,
-  "item-id": number | null,
-  rank: number,
-  title: string,
-  "item-type": string | null,
-  "section-slug": string | null,
-  "tag-slug": string | null,
-  id: number,
-  "parent-id": number | null,
-  url: string,
-  "entity-name": string | null,
-  "collection-slug": string | null,
-  "section-name": string | null,
+  "tag-name": string | null;
+  "entity-properties": string | null;
+  "collection-id": number | null;
+  "entity-slug": string | null;
+  "item-id": number | null;
+  rank: number;
+  title: string;
+  "item-type": string | null;
+  "section-slug": string | null;
+  "tag-slug": string | null;
+  id: number;
+  "parent-id": number | null;
+  url: string;
+  "entity-name": string | null;
+  "collection-slug": string | null;
+  "section-name": string | null;
   data?: {
-    color: string,
-    link?: string
-  }
+    color: string;
+    link?: string;
+  };
 }
 
 export interface SlotType {
