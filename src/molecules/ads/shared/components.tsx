7da6276da6cee30ca7e33eb68c496baa943f2d01
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-05-22 20:56:31 +0530

    disabled dark background for story ad, showing it for top & bottom ads

--- src/molecules/ads/shared/components.tsx
+++ src/molecules/ads/shared/components.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const AdWrapper = styled.div`
+export const AdWrapper = styled.div<{ darkBackground?: boolean }>`
   text-align: center;
-  background-color: ${(props) => props.theme.color.mono1};
+  background-color: ${(props) => (props.darkBackground ? props.theme.color.mono1 : null)};
 `;
