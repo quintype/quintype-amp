c410940c85757d7d13e148ed97455d348f4cda64
Ramsharan Gorur Jayaraman <sharangj@gmail.com>
2020-05-26 19:02:46 +0530

    Add app id to facebook social share

--- src/atoms/social-share-icon/social-share-icon.tsx
+++ src/atoms/social-share-icon/social-share-icon.tsx
@@ -12,8 +12,10 @@ export const SocialShareIcon = ({ type, width = "40", height = "40", styles = {}
           src="https://cdn.ampproject.org/v0/amp-social-share-0.1.js"
         />
       </Helmet>
-      {type === 'facebook' && <amp-social-share style={styles} type={type} width={width} height={height} data-param-app_id={appId}}
-      {type !== 'facebook' && <amp-social-share style={styles} type={type} width={width} height={height} />
+      {type === "facebook" && (
+        <amp-social-share style={styles} type={type} width={width} height={height} data-param-app_id={appId} />
+      )}
+      {type !== "facebook" && <amp-social-share style={styles} type={type} width={width} height={height} />}
     </Fragment>
   );
 };
