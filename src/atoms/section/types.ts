7c3399a97e4438525bcd1bbc25c8f9d31e62e1ea
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-06-01 18:51:21 +0530

    cleanup

--- src/atoms/section/types.ts
+++ src/atoms/section/types.ts
@@ -2,9 +2,4 @@ import { Section } from "../../types/story";
 
 export interface SectionProps {
   section: Section;
-  stylesFromConfig?: object;
-}
-
-export interface StyledSectionTypes {
-  stylesFromConfig: object;
 }
