91da35ffcaeeabd63ef4a0f62846accfe044edf2
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-06-02 09:34:40 +0530

    headerCard cleanup

--- src/molecules/header-card/header-card.tsx
+++ src/molecules/header-card/header-card.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import styled from "styled-components";
 import { withStoryAndConfig } from "../../context";
-import { DefaultHeaderCard } from "./container-components";
+import { Author, Section, Spacer } from "../../atoms";
+import { HeroImage } from "../hero-image";
+import { SocialShareHeader } from "../social-share-header";
+import { DateLastPublished } from "../date";
 import { HeaderCardTypes } from "./types";
 
 const HeaderCardBase = ({ story, config }: HeaderCardTypes) => {
@@ -11,6 +15,39 @@ const HeaderCardBase = ({ story, config }: HeaderCardTypes) => {
   );
 };
 
-const HeaderCard = withStoryAndConfig(HeaderCardBase);
+export const HeaderCard = withStoryAndConfig(HeaderCardBase);
 
-export { HeaderCard };
+const Headline = styled.h1`
+  font-family: ${(props) => props.theme.font.family.primary};
+  font-size: ${(props) => props.theme.font.size.l};
+  color: ${(props) => props.theme.color.black};
+  line-height: ${(props) => props.theme.font.lineHeight.level2};
+  margin: 0;
+  font-weight: bold;
+`;
+const HeaderCardContainer = styled.div`
+  padding: 0 ${(props) => props.theme.spacing.s};
+  border-bottom: ${(props) => `1px solid ${props.theme.color.black}`};
+`;
+
+const DefaultHeaderCard = ({ story, config }: HeaderCardTypes) => {
+  const { publisherConfig } = config;
+  return (
+    <div>
+      <HeroImage />
+      <Spacer token="xs" />
+      <HeaderCardContainer>
+        <Section section={story.sections[0]} />
+        <Spacer token="xs" />
+        <Headline>{story.headline}</Headline>
+        <Spacer token="s" />
+        <Author authors={story.authors} prepend="By " />
+        <Spacer token="xxs" />
+        <DateLastPublished />
+        <Spacer token="m" />
+        <SocialShareHeader fbAppId={publisherConfig.facebook && publisherConfig.facebook["app-id"]} />
+        <Spacer token="s" />
+      </HeaderCardContainer>
+    </div>
+  );
+};
