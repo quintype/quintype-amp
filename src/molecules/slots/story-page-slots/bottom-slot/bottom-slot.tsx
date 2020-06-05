0b357a533c9d31259bd7b342f3d9640c8767e2bd
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-06-05 15:06:05 +0530

    fix: change story slots to accept render props

--- src/molecules/slots/story-page-slots/bottom-slot/bottom-slot.tsx
+++ src/molecules/slots/story-page-slots/bottom-slot/bottom-slot.tsx
@@ -1,16 +1,13 @@
-import React from "react";
-import { Slot } from "../../../../atoms";
-import { withConfig } from "../../../../context";
+import { withStoryAndConfig } from "../../../../context";
 import { getSlotFromConfig } from "../../helpers";
-import { SlotType } from "../../../../types/config";
 
-const BottomSlotBase = ({ config }) => {
-  const { ampHtml, script, styles }: SlotType = getSlotFromConfig({
+const BottomSlotBase = ({ story, config }) => {
+  const bottomSlotRenderProp = getSlotFromConfig({
     config,
     slotType: "story",
     slotName: "bottom-slot"
   });
-  return ampHtml ? <Slot ampHtml={ampHtml} script={script} styles={styles} /> : null;
+  return bottomSlotRenderProp && bottomSlotRenderProp({ story, config });
 };
 
-export const BottomSlot = withConfig(BottomSlotBase);
+export const BottomSlot = withStoryAndConfig(BottomSlotBase);
