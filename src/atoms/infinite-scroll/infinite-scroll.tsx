8f2b5bd8d076a256ac3012a50d0b2a2a1db8729c
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-06-12 09:13:08 +0530

    feat: infinite scroll

---
+++ src/atoms/infinite-scroll/infinite-scroll.tsx
@@ -0,0 +1,25 @@
+import React, { Fragment } from "react";
+import { Helmet } from "react-helmet";
+import { withStoryAndConfig } from "../../context";
+
+const InfiniteScrollBase = ({ story }) => {
+  const { "story-content-id": storyId } = story;
+  // const { "sketches-host": host } = config.publisherConfig;
+  // host has to be https and on the same origin
+  // const jsonConfigUrl = `${host}/amp/api/v1/amp-infinite-scroll?story-id=${storyId}`;
+  const jsonConfigUrl = `https://vikatan-gamma.quintype.io/amp/api/v1/amp-infinite-scroll?story-id=${storyId}`; // TEMPORARILY HARDCODING HOST
+  return (
+    <Fragment>
+      <Helmet>
+        <script
+          async={undefined}
+          custom-element="amp-next-page"
+          src="https://cdn.ampproject.org/v0/amp-next-page-1.0.js"
+        />
+      </Helmet>
+      <amp-next-page src={jsonConfigUrl} />
+    </Fragment>
+  );
+};
+
+export const InfiniteScroll = withStoryAndConfig(InfiniteScrollBase);
