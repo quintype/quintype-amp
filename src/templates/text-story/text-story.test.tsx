819b74525a4171df66b90073424f70e8d6c8cd35
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-06-17 09:07:19 +0530

    test: add infinite scroll test inside text story

--- src/templates/text-story/text-story.test.tsx
+++ src/templates/text-story/text-story.test.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import { shallow } from "enzyme";
 import { TextStory } from "./text-story";
 import { RelatedStories } from "../../molecules";
-
+import { InfiniteScroll } from "../../atoms";
 import { textStory, config, relatedStories } from "../../__fixtures__";
+import cloneDeep from "lodash.clonedeep";
+
 describe("TextStory Template", () => {
   it("should render", () => {
     const wrapper = shallow(<TextStory story={textStory} config={config} relatedStories={relatedStories} />);
@@ -12,6 +14,18 @@ describe("TextStory Template", () => {
   it("should render related stories component", () => {
     const wrapper = shallow(<TextStory story={textStory} config={config} relatedStories={relatedStories} />);
     expect(wrapper.find(RelatedStories).length).toBe(1);
+  });
+  it("should render infinite scroll component if exists", () => {
+    const modifiedConfig = cloneDeep(config);
+    modifiedConfig.ampConfig["related-collection-id"] = "1234"; // !!!! change to infinite-scroll-collection-id later
+    const wrapper = shallow(<TextStory story={textStory} config={modifiedConfig} relatedStories={relatedStories} />);
+    expect(wrapper.find(InfiniteScroll).length).toBe(1);
+  });
+  it("should not render infinite scroll component if infinite-scroll-collection-id not passed in ampconfig", () => {
+    const modifiedConfig = cloneDeep(config);
+    modifiedConfig.ampConfig["related-collection-id"] = null; // !!!! change to infinite-scroll-collection-id later
+    const wrapper = shallow(<TextStory story={textStory} config={modifiedConfig} relatedStories={relatedStories} />);
+    expect(wrapper.find(InfiniteScroll).length).toBe(0);
   });
   it("should call relatedStoriesRender prop when passed to opts", () => {
     const relatedStoriesRender = jest.fn();
