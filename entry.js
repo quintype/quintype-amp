8fdf8d37317caada7a3d43c2ac92c2c09394de8e
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-06-23 16:33:32 +0530

    Updates

--- entry.js
+++ entry.js
@@ -22,11 +22,62 @@
 
     import './styles/iframe.css';
 
-  import Component0 from '../src/atoms/author/author.tsx';
-reactComponents['Author'] = Component0;
+  import Component0 from '../src/atoms/story-elements/also-read/also-read.tsx';
+reactComponents['AlsoRead'] = Component0;
 
-import Component1 from '../src/atoms/story-elements/blurb/blurb.tsx';
-reactComponents['Blurb'] = Component1;
+import Component1 from '../src/atoms/story-elements/text/answer/answer.tsx';
+reactComponents['Answer'] = Component1;
 
-import Component2 from '../src/molecules/header-card/header-card.tsx';
-reactComponents['HeaderCard'] = Component2;
+import Component2 from '../src/atoms/author/author.tsx';
+reactComponents['Author'] = Component2;
+
+import Component3 from '../src/atoms/story-elements/text/bigfact/bigfact.tsx';
+reactComponents['Bigfact'] = Component3;
+
+import Component4 from '../src/atoms/story-elements/blockquote/blockquote.tsx';
+reactComponents['BlockQuote'] = Component4;
+
+import Component5 from '../src/atoms/story-elements/blurb/blurb.tsx';
+reactComponents['Blurb'] = Component5;
+
+import Component6 from '../src/atoms/story-elements/daily-motion-element/daily-motion-element.tsx';
+reactComponents['DailyMotionElement'] = Component6;
+
+import Component7 from '../src/atoms/story-elements/embed/embed.tsx';
+reactComponents['Embed'] = Component7;
+
+import Component8 from '../src/atoms/story-elements/facebook-element/facebook-element.tsx';
+reactComponents['FacebookElement'] = Component8;
+
+import Component9 from '../src/molecules/header-card/header-card.tsx';
+reactComponents['HeaderCard'] = Component9;
+
+import Component10 from '../src/atoms/story-elements/image-element/image-element.tsx';
+reactComponents['ImageElement'] = Component10;
+
+import Component11 from '../src/atoms/story-elements/image-gallery-element/image-gallery-element.tsx';
+reactComponents['ImageGalleryElement'] = Component11;
+
+import Component12 from '../src/atoms/story-elements/instagram-element/instagram-element.tsx';
+reactComponents['InstagramElement'] = Component12;
+
+import Component13 from '../src/atoms/story-elements/text/question/question.tsx';
+reactComponents['Question'] = Component13;
+
+import Component14 from '../src/atoms/story-elements/text/summary/summary.tsx';
+reactComponents['Summary'] = Component14;
+
+import Component15 from '../src/atoms/story-elements/text/text.tsx';
+reactComponents['Text'] = Component15;
+
+import Component16 from '../src/atoms/story-elements/title/title.tsx';
+reactComponents['Title'] = Component16;
+
+import Component17 from '../src/atoms/story-elements/twitter-element/twitter-element.tsx';
+reactComponents['TwitterElement'] = Component17;
+
+import Component18 from '../src/atoms/story-elements/vidible-element/vidible-element.tsx';
+reactComponents['VidibleElement'] = Component18;
+
+import Component19 from '../src/atoms/story-elements/youtube/youtube.tsx';
+reactComponents['YouTube'] = Component19;
