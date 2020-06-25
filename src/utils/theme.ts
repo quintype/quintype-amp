a8c7f92ad2517db373acd2232728e2c581a7cce1
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-06-25 23:07:29 +0530

    test: theme tokens

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,23 +1,22 @@
 import { AMPConfig } from "../types/config";
-
-const exists = (text?: string) => !!(text && text.length);
+import get from "lodash.get";
 
 export const getTokensFromAMPConfig = (config: AMPConfig) => {
   const { colors } = config;
   const { fonts } = config;
   return {
     color: {
-      ...(exists(colors.primary) && { primaryColor: colors.primary }),
-      ...(exists(colors.secondary) && { secondaryColor: colors.secondary }),
-      ...(exists(colors["header-background"]) && { headerBackground: colors["header-background"] }),
-      ...(exists(colors["footer-background"]) && { footerBackground: colors["footer-background"] }),
-      ...(exists(colors["footer-text-color"]) && { footerTextColor: colors["footer-text-color"] }),
-      ...(exists(colors["section-text-color"]) && { sectionTextColor: colors["section-text-color"] })
+      ...(get(colors, ["primary"], null) && { primaryColor: colors.primary }),
+      ...(get(colors, ["secondary"], null) && { secondaryColor: colors.secondary }),
+      ...(get(colors, ["header-background"], null) && { headerBackground: colors["header-background"] }),
+      ...(get(colors, ["footer-background"], null) && { footerBackground: colors["footer-background"] }),
+      ...(get(colors, ["footer-text-color"], null) && { footerTextColor: colors["footer-text-color"] }),
+      ...(get(colors, ["section-text-color"], null) && { sectionTextColor: colors["section-text-color"] })
     },
     font: {
       family: {
-        ...(exists(fonts.primary.family) && { primary: fonts.primary.family }),
-        ...(exists(fonts.secondary.family) && { secondary: fonts.secondary.family })
+        ...(get(fonts, ["primary", "family"], null) && { primary: fonts.primary.family }),
+        ...(get(fonts, ["secondary", "family"], null) && { secondary: fonts.secondary.family })
       }
     }
   };
