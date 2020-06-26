e5ee84c4d70dfe2eaea75aa75433bf23600b964a
Amogh S <amogh1773@gmail.com>
2020-06-26 11:12:46 +0530

    Tq fit prod bugfix (#215)
    * fix: app crashes when theme tokens not set in config
    
    * chore(release): 1.3.0-tq-fit-prod-bugfix.0
    
    * test: theme tokens
    
    * chore(release): 1.3.0
    
    Co-authored-by: Amogh Sahasrabhojanee <amogh@quintype.com>

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,23 +1,22 @@
 import { AMPConfig } from "../types/config";
-
-const isEmpty = (text: string) => (text.length < 1 ? true : false);
+import get from "lodash.get";
 
 export const getTokensFromAMPConfig = (config: AMPConfig) => {
   const { colors } = config;
   const { fonts } = config;
   return {
     color: {
-      ...(!isEmpty(colors.primary) && { primaryColor: colors.primary }),
-      ...(!isEmpty(colors.secondary) && { secondaryColor: colors.secondary }),
-      ...(!isEmpty(colors["header-background"]) && { headerBackground: colors["header-background"] }),
-      ...(!isEmpty(colors["footer-background"]) && { footerBackground: colors["footer-background"] }),
-      ...(!isEmpty(colors["footer-text-color"]) && { footerTextColor: colors["footer-text-color"] }),
-      ...(!isEmpty(colors["section-text-color"]) && { sectionTextColor: colors["section-text-color"] })
+      ...(get(colors, ["primary"], null) && { primaryColor: colors.primary }),
+      ...(get(colors, ["secondary"], null) && { secondaryColor: colors.secondary }),
+      ...(get(colors, ["header-background"], null) && { headerBackground: colors["header-background"] }),
+      ...(get(colors, ["footer-background"], null) && { footerBackground: colors["footer-background"] }),
+      ...(get(colors, ["footer-text-color"], null) && { footerTextColor: colors["footer-text-color"] }),
+      ...(get(colors, ["section-text-color"], null) && { sectionTextColor: colors["section-text-color"] })
     },
     font: {
       family: {
-        ...(!isEmpty(fonts.primary.family) && { primary: fonts.primary.family }),
-        ...(!isEmpty(fonts.secondary.family) && { secondary: fonts.secondary.family })
+        ...(get(fonts, ["primary", "family"], null) && { primary: fonts.primary.family }),
+        ...(get(fonts, ["secondary", "family"], null) && { secondary: fonts.secondary.family })
       }
     }
   };
