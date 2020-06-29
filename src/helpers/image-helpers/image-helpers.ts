7b7e83da2d0af6b7b7290aa579d48525fe85af29
Shraddha Kesari <shraddha.kesari@gmail.com>
2020-06-29 14:14:42 +0530

    Removes image compression

--- src/helpers/image-helpers/image-helpers.ts
+++ src/helpers/image-helpers/image-helpers.ts
@@ -1,10 +1,10 @@
 import quintypeJs from "quintype-js";
 
 export const focusedImagePath = ({ opts, slug, metadata, imgAspectRatio, cdnImage }) => {
-  let auto = ["format"];
-  const supportsCompression = !/\.gif/.test(slug);
-  if (supportsCompression) auto = auto.concat(["compress"]);
-  opts = Object.assign({ auto, w: "1200" }, opts);
+  const auto = ["format"];
+  // const supportsCompression = !/\.gif/.test(slug);
+  // if (supportsCompression) auto = auto.concat(["compress"]);
+  opts = Object.assign({ auto, w: "1600" }, opts);
   const path = new quintypeJs.FocusedImage(slug, metadata).path(imgAspectRatio, opts);
   const hostWithProtocol = /^https:\/\//.test(cdnImage) ? cdnImage : `https://${cdnImage}`;
   return `${hostWithProtocol}/${path}`;
