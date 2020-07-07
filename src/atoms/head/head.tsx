7d3fdd3825cdc096e42ed618336f03db97b597f0
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-07-07 16:16:18 +0530

    fix: remove <Link/> atom since <Head/> can be used to do the same thing

--- src/atoms/head/head.tsx
+++ src/atoms/head/head.tsx
@@ -2,6 +2,19 @@ import React, { Fragment } from "react";
 import { Helmet } from "react-helmet";
 import { HeadTypes } from "./types";
 
+/**
+ *
+ * Head component is a simple wrapper for the [react-helmet](https://www.npmjs.com/package/react-helmet) `<Helmet />` component.
+ * It puts child items in the document head. Scripts, styles, SEO meta tags, link tag etc. can be given as children to this component.
+ * Can be used multiple times or nested.
+ *
+ * @param {Object} params object containing parameters passed to the render prop
+ * @param {Object} params.children child components
+ *
+ * @category Atoms
+ * @module Head
+ */
+
 export const Head = ({ children }: HeadTypes) => (
   <Fragment>
     <Helmet>{children}</Helmet>
--- src/molecules/slots/tests/slots-validation.test.tsx
+++ src/molecules/slots/tests/slots-validation.test.tsx
@@ -3,7 +3,7 @@
  */
 
 import React from "react";
-import { Layout, Link } from "../../../atoms";
+import { Layout, Head } from "../../../atoms";
 import { TopSlot } from "../story-page-slots/top-slot";
 import { BottomSlot } from "../story-page-slots/bottom-slot";
 import { textStory, config } from "../../../__fixtures__";
@@ -19,7 +19,9 @@ const output = async (layout) => {
 test("Slots should pass ampValidation", async () => {
   const dummyLayout = (
     <Layout story={textStory} config={config}>
-      <Link rel="canonical" href="." />
+      <Head>
+        <link rel="canonical" href="." />
+      </Head>
       <TopSlot />
       <BottomSlot />
     </Layout>
