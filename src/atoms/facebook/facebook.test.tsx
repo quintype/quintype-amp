6c567b22c8aa66330f37d8b9097087dc175692f5
Shraddha Kesari <shraddha.kesari@gmail.com>
2020-07-09 16:38:32 +0530

    Fixes some failing tests

--- src/atoms/facebook/facebook.test.tsx
+++ src/atoms/facebook/facebook.test.tsx
@@ -4,11 +4,15 @@ import { Facebook } from "./facebook";
 
 describe("Facebook", () => {
   it("should render facebook element", () => {
-    const wrapper = shallow(<Facebook data-href="https://www.facebook.com/ParksCanada/posts/1712989015384373" />);
+    const wrapper = shallow(
+      <Facebook data-href="https://www.facebook.com/ParksCanada/posts/1712989015384373" title="facebook" />
+    );
     expect(wrapper.find("amp-facebook").length).toBe(1);
   });
   it("should render facebook element with default params", () => {
-    const wrapper = shallow(<Facebook data-href="https://www.facebook.com/ParksCanada/posts/1712989015384373" />);
+    const wrapper = shallow(
+      <Facebook data-href="https://www.facebook.com/ParksCanada/posts/1712989015384373" title="facebook" />
+    );
     expect(wrapper.find("amp-facebook").prop("width")).toBe("16");
     expect(wrapper.find("amp-facebook").prop("height")).toBe("9");
     expect(wrapper.find("amp-facebook").prop("layout")).toBe("responsive");
@@ -20,6 +24,7 @@ describe("Facebook", () => {
         width="250"
         height="250"
         layout="fixed"
+        title="facebook"
       />
     );
     expect(wrapper.find("amp-facebook").prop("width")).toBe("250");
@@ -27,7 +32,11 @@ describe("Facebook", () => {
   });
   it("should render facebook video", () => {
     const wrapper = shallow(
-      <Facebook data-href="https://www.facebook.com/nasaearth/videos/10155187938052139" data-embed-as="video" />
+      <Facebook
+        data-href="https://www.facebook.com/nasaearth/videos/10155187938052139"
+        data-embed-as="video"
+        title="facebook"
+      />
     );
     expect(wrapper.find("amp-facebook").prop("data-embed-as")).toBe("video");
   });
@@ -36,6 +45,7 @@ describe("Facebook", () => {
       <Facebook
         data-href="https://www.facebook.com/zuck/posts/10102735452532991?comment_id=1070233703036185"
         data-embed-type="comment"
+        title="facebook"
       />
     );
     expect(wrapper.find("amp-facebook").prop("data-embed-type")).toBe("comment");
