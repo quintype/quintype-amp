ff825824ab92e6f5fdd4e2b3c3f57015dda725dc
Shraddha Kesari <shraddha.kesari@gmail.com>
2020-07-14 17:34:12 +0530

    Tests for the uncovered lines of code (#227)
    * fixes tests
    
    * Revert "fixes tests"
    
    This reverts commit 9d1d8ead48062a0e8642ebd56ae4e7a7e79368b1.
    
    * Adds tests which aren't covered

--- src/atoms/footer/footer.test.tsx
+++ src/atoms/footer/footer.test.tsx
@@ -7,4 +7,17 @@ describe("Footer", () => {
     const wrapper = shallow(<BaseFooter />);
     expect(wrapper).toMatchSnapshot();
   });
+  it("should render children", () => {
+    const wrapper = shallow(
+      <BaseFooter>
+        <amp-img
+          src="https://images.unsplash.com/photo-1558980395-be8a5fcb4251?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          width="400"
+          height="300"
+          alt="a sample image"
+        />
+      </BaseFooter>
+    );
+    expect(wrapper.find("amp-img").length).toBe(1);
+  });
 });
