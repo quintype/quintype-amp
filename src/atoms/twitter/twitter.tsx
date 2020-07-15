85bdc121b16a9df61344a843b610cbaefe4418ca
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-07-15 15:18:23 +0530

    fix(opts restructure): twitter

--- src/atoms/twitter/twitter.tsx
+++ src/atoms/twitter/twitter.tsx
@@ -1,22 +1,16 @@
 import React, { Fragment } from "react";
 import { TwitterTypes } from "./types";
 import { Helmet } from "react-helmet";
-import styled from "styled-components";
 
-export const StyledTwitter = styled.div.attrs(({ style }: { style?: object }) => ({
-  style
-}))``;
-
-export const Twitter = ({ inlineStyles, ...props }: TwitterTypes) => {
-  const { width, height, layout, title } = props;
-  const setDefaultLayout = !width || !height || !layout || !title;
+export const Twitter = (props: TwitterTypes) => {
+  const { width, height, layout } = props;
+  const setDefaultLayout = !width || !height || !layout;
   const componentProps: TwitterTypes = setDefaultLayout
     ? {
         ...props,
         width: "390",
         height: "330",
-        layout: "responsive",
-        title: "twitter"
+        layout: "responsive"
       }
     : props;
   return (
@@ -24,9 +18,7 @@ export const Twitter = ({ inlineStyles, ...props }: TwitterTypes) => {
       <Helmet>
         <script async={undefined} custom-element="amp-twitter" src="https://cdn.ampproject.org/v0/amp-twitter-0.1.js" />
       </Helmet>
-      <StyledTwitter style={inlineStyles}>
-        <amp-twitter {...componentProps} />
-      </StyledTwitter>
+      <amp-twitter {...componentProps} />
     </Fragment>
   );
 };
--- src/atoms/twitter/types.ts
+++ src/atoms/twitter/types.ts
@@ -3,6 +3,5 @@ import { Common } from "../common-types";
 export interface TwitterTypes extends Common {
   "data-tweetid": string;
   children?: any;
-  inlineStyles?: object;
-  title: string;
+  title?: string;
 }
