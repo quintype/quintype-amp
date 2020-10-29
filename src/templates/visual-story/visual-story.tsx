ef142f0dfb3e1612e2cc07903bde9957437f4c7d
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-10-29 10:03:08 +0530

    fix: formatting

--- src/templates/visual-story/visual-story.tsx
+++ src/templates/visual-story/visual-story.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import get from "lodash/get";
 import { Layout, StoryElement, VisualImage, VisualText } from "../../atoms";
 import Helmet from "react-helmet";
@@ -11,77 +11,62 @@ import { ConfigProvider } from "../../context/config/config-context";
 import { ImageElement } from "../../atoms/story-elements";
 
 export const VisualStory = ({ story, config }) => {
-    const publisherLogo = get(config.publisherConfig["publisher-settings"]["publisher-logo"], "")
-    const publisherTitle = get(config.publisherConfig["publisher-settings"].title, "")
+  const publisherLogo = get(config.publisherConfig["publisher-settings"]["publisher-logo"], "");
+  const publisherTitle = get(config.publisherConfig["publisher-settings"].title, "");
 
-    return (
-        <>
-            <Helmet>
-                <script async={undefined} custom-element="amp-story" src="https://cdn.ampproject.org/v0/amp-story-1.0.js" />
-            </Helmet>
-            <amp-story
-                standalone=""
-                publisher-logo-src={publisherLogo}
-                title={story.headline}
-                publisher={publisherTitle}
-                poster-portrait-src={story["hero-image-s3-key"]}
-            >
-                <amp-story-page id="cover">
-                    <VisualImage story={story} config={config} />
-                    <VerticalTemplateWrapper>
-                        <Layout story={story} config={config}>
-                            <HeaderCard story={story} config={config} />
-                        </Layout>
-                    </VerticalTemplateWrapper>
-                </amp-story-page>
-                <VisualFullStoryContent story={story} config={config} />
-                {/* <VerticalTemplateWrapper> */}
-                {/* <Layout story={story} config={config}> */}
-
-                <RelatedStories />
-                {/* </Layout> */}
-                {/* </VerticalTemplateWrapper> */}
-            </amp-story>
-        </>
-
-    );
+  return (
+    <>
+      <Helmet>
+        <script async={undefined} custom-element="amp-story" src="https://cdn.ampproject.org/v0/amp-story-1.0.js" />
+      </Helmet>
+      <amp-story
+        standalone=""
+        publisher-logo-src={publisherLogo}
+        title={story.headline}
+        publisher={publisherTitle}
+        poster-portrait-src={story["hero-image-s3-key"]}>
+        <amp-story-page id="cover">
+          <VisualImage story={story} config={config} />
+          <VerticalTemplateWrapper>
+            <Layout story={story} config={config}>
+              <HeaderCard story={story} config={config} />
+            </Layout>
+          </VerticalTemplateWrapper>
+        </amp-story-page>
+        <VisualFullStoryContent story={story} config={config} />
+        <RelatedStories />
+      </amp-story>
+    </>
+  );
 };
 
 export const ImageTemplateWrapper = ({ children }) => {
-    return <amp-story-grid-layer template="fill">{children}</amp-story-grid-layer>
-}
-
-export const VerticalTemplateWrapper = ({ children }) => {
-    return <amp-story-grid-layer template="vertical">{children}</amp-story-grid-layer>
-}
-
-export const StoryElementTemplateWrapper = ({ children }) => {
-    return <amp-story-grid-layer template="thirds">{children}</amp-story-grid-layer>
-}
-
-export const VisualFullStoryContent = ({ story, config }) => {
-    return story.cards.map((card) => {
-        const imageElement = card["story-elements"].map(element => element.type).includes("image")
-        // console.log(imageElement, "<----imageElement");
-        // console.log(card, "<----card");
-        return <amp-story-page id={card.id} key={card.id}>
-            <Layout story={story} config={config}>
-                {card["story-elements"].map((element) => (
-                    <ImageTemplateWrapper key={element.id}>
-                        <StoryElement key={element.id} element={element} />
-                    </ImageTemplateWrapper>
-                ))
-                }
-                {/* {card["story-elements"].map((element) => (
-                    <StoryElementTemplateWrapper key={element.id}>
-                        <StoryElement key={element.id} element={element} grid-area="middle-third" />
-                    </StoryElementTemplateWrapper>
-                ))
-                } */}
-            </Layout>
-            {/* : ""
-            } */}
-        </amp-story-page>;
-    })
+  return <amp-story-grid-layer template="fill">{children}</amp-story-grid-layer>;
 };
 
+export const VerticalTemplateWrapper = ({ children }) => {
+  return <amp-story-grid-layer template="vertical">{children}</amp-story-grid-layer>;
+};
+
+export const StoryElementTemplateWrapper = ({ children }) => {
+  return <amp-story-grid-layer template="thirds">{children}</amp-story-grid-layer>;
+};
+
+export const VisualFullStoryContent = ({ story, config }) => {
+  return story.cards.map((card) => {
+    const imageElement = card["story-elements"].map((element) => element.type).includes("image");
+    // console.log(imageElement, "<----imageElement");
+    // console.log(card, "<----card");
+    return (
+      <amp-story-page id={card.id} key={card.id}>
+        <Layout story={story} config={config}>
+          {card["story-elements"].map((element) => (
+            <ImageTemplateWrapper key={element.id}>
+              <StoryElement key={element.id} element={element} />
+            </ImageTemplateWrapper>
+          ))}
+        </Layout>
+      </amp-story-page>
+    );
+  });
+};
