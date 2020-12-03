3d4438ce575dc1bcd9fd3d84d721d4ffa0cb1efc
Amogh Sahasrabhojanee <amogh@quintype.com>
2020-12-03 11:31:33 +0530

    test(ga): do not render GA component if config is not present

--- src/atoms/google-analytics/google-analytics.test.tsx
+++ src/atoms/google-analytics/google-analytics.test.tsx
@@ -6,6 +6,14 @@ import { Analytics } from "../analytics";
 const config = {
   ampConfig: { "google-analytics-tracking-id": "UX-656565" }
 };
+const configWithoutGa1 = {
+  ampConfig: {}
+};
+const configWithoutGa2 = {
+  ampConfig: {
+    "google-analytics-tracking-id": ""
+  }
+};
 describe("Google Analytics", () => {
   it("should render", () => {
     const wrapper = shallow(<GoogleAnalyticsBase config={config} />);
@@ -24,5 +32,13 @@ describe("Google Analytics", () => {
   it("should have type prop", () => {
     const wrapper = shallow(<GoogleAnalyticsBase config={config} />);
     expect(wrapper.find(Analytics).prop("type")).toEqual("googleanalytics");
+  });
+  it("should not render if GA config isn't present", () => {
+    const wrapper = shallow(<GoogleAnalyticsBase config={configWithoutGa1} />);
+    expect(wrapper.find(Analytics).length).toBe(0);
+  });
+  it("should not render if GA config is empty", () => {
+    const wrapper = shallow(<GoogleAnalyticsBase config={configWithoutGa2} />);
+    expect(wrapper.find(Analytics).length).toBe(0);
   });
 });
