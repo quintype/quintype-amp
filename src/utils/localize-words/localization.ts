669548c2d3e01d655d3704eedf99ad30afb6310f
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2022-04-25 16:06:53 +0530

    pr changes

--- src/utils/localize-words/localization.ts
+++ src/utils/localize-words/localization.ts
@@ -1,9 +1,9 @@
 import get from "lodash.get";
 import { Config } from "../../types/config";
 
-export const getLocalizedWord = (config: Config | undefined, word: string, fallback: string) => {
+export const getLocalizedWord = (config: Config | undefined, label: string, fallback: string) => {
   const optsConfig = get(config, ["opts"], {});
   const localizationConfig =
     typeof optsConfig.localization === "function" ? optsConfig.localization(config) : optsConfig.localization;
-  return localizationConfig ? localizationConfig[word] : fallback;
+  return localizationConfig ? localizationConfig[label] : fallback;
 };
