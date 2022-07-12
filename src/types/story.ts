ea0bd8311d2e5cb304ed082a26923006b81ea11a
Amogh Sahasrabhojanee <amogh1773@gmail.com>
2022-07-12 11:29:46 +0530

    merge master

--- src/types/story.ts
+++ src/types/story.ts
@@ -178,6 +178,10 @@ export interface StoryElementMetadata {
   question?: string;
   answer?: string;
   "has-header"?: boolean;
+  "cta-title"?: string;
+  "cta-url"?: string;
+  "open-in-new-tab"?: boolean;
+  "no-follow"?: boolean;
 }
 
 export interface LinkedStory {
