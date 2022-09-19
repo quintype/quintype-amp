d8916c354e4d93c6a6509657480a0dd5af0839bf
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2022-09-19 14:28:45 +0530

    feat: add video support for webstories (#389)
    * chore(release): 2.4.29-web-stories-video.1

---
+++ src/atoms/video-web-story/video-web-story.tsx
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+import Helmet from "react-helmet";
+import atob from "atob";
+import { withConfig } from "../../context";
+
+export const VideoWebStoryBase = ({ config, videoElement, imageElement }) => {
+  const videoUrl = videoElement && atob(`${videoElement["embed-js"]}`);
+  const format = videoUrl.split(".")[3];
+  const poster = imageElement ? `https://${config.publisherConfig["cdn-image"]}/${imageElement["image-s3-key"]}` : null;
+  return (
+    <Fragment>
+      <Helmet>
+        <script async={undefined} custom-element="amp-video" src="https://cdn.ampproject.org/v0/amp-video-0.1.js" />
+      </Helmet>
+      <amp-story-grid-layer template="fill">
+        <amp-video autoplay="" poster={poster} height="auto" layout="responsive">
+          <source src={videoUrl} type={`video/${format}`} />
+        </amp-video>
+      </amp-story-grid-layer>
+    </Fragment>
+  );
+};
+
+export const VideoWebStory = withConfig(VideoWebStoryBase);
