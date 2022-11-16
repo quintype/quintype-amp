64af66d229d3fa8eb5b28b193385e2a6190b5bc9
Athira MR <athiramraju001@gmail.com>
2022-11-16 11:32:17 +0530

    Updates

--- entry.js
+++ entry.js
@@ -22,119 +22,122 @@
 
     import './styles/iframe.css';
 
-  import Component0 from '../src/molecules/date/published/date-last-published.tsx';
-reactComponents['DateLastPublished'] = Component0;
+  import Component0 from '../src/molecules/date/published/date-first-published.tsx';
+reactComponents['DateFirstPublished'] = Component0;
 
-import Component1 from '../src/atoms/date-time/date-time.tsx';
-reactComponents['DateTime'] = Component1;
+import Component1 from '../src/molecules/date/published/date-last-published.tsx';
+reactComponents['DateLastPublished'] = Component1;
 
-import Component2 from '../src/molecules/date/updated/date-updated.tsx';
-reactComponents['DateUpdated'] = Component2;
+import Component2 from '../src/atoms/date-time/date-time.tsx';
+reactComponents['DateTime'] = Component2;
 
-import Component3 from '../src/templates/generic-story/generic-story.tsx';
-reactComponents['GenericStory'] = Component3;
+import Component3 from '../src/molecules/date/updated/date-updated.tsx';
+reactComponents['DateUpdated'] = Component3;
 
-import Component4 from '../src/atoms/head/head.tsx';
-reactComponents['Head'] = Component4;
+import Component4 from '../src/templates/generic-story/generic-story.tsx';
+reactComponents['GenericStory'] = Component4;
 
-import Component5 from '../src/molecules/hero-image/hero-image.tsx';
-reactComponents['HeroImage'] = Component5;
+import Component5 from '../src/atoms/head/head.tsx';
+reactComponents['Head'] = Component5;
 
-import Component6 from '../src/atoms/image/image.tsx';
-reactComponents['Image'] = Component6;
+import Component6 from '../src/molecules/hero-image/hero-image.tsx';
+reactComponents['HeroImage'] = Component6;
 
-import Component7 from '../src/atoms/layout/layout.tsx';
-reactComponents['Layout'] = Component7;
+import Component7 from '../src/atoms/image/image.tsx';
+reactComponents['Image'] = Component7;
 
-import Component8 from '../src/templates/live-blog/live-blog.tsx';
-reactComponents['LiveBlog'] = Component8;
+import Component8 from '../src/atoms/layout/layout.tsx';
+reactComponents['Layout'] = Component8;
 
-import Component9 from '../src/atoms/story-elements/also-read/also-read.tsx';
-reactComponents['AlsoRead'] = Component9;
+import Component9 from '../src/templates/live-blog/live-blog.tsx';
+reactComponents['LiveBlog'] = Component9;
 
-import Component10 from '../src/atoms/story-elements/text/answer/answer.tsx';
-reactComponents['Answer'] = Component10;
+import Component10 from '../src/atoms/story-elements/also-read/also-read.tsx';
+reactComponents['AlsoRead'] = Component10;
 
-import Component11 from '../src/atoms/author/author.tsx';
-reactComponents['Author'] = Component11;
+import Component11 from '../src/atoms/story-elements/text/answer/answer.tsx';
+reactComponents['Answer'] = Component11;
 
-import Component12 from '../src/atoms/story-elements/text/bigfact/bigfact.tsx';
-reactComponents['Bigfact'] = Component12;
+import Component12 from '../src/atoms/author/author.tsx';
+reactComponents['Author'] = Component12;
 
-import Component13 from '../src/atoms/story-elements/blockquote/blockquote.tsx';
-reactComponents['BlockQuote'] = Component13;
+import Component13 from '../src/atoms/story-elements/text/bigfact/bigfact.tsx';
+reactComponents['Bigfact'] = Component13;
 
-import Component14 from '../src/atoms/story-elements/blurb/blurb.tsx';
-reactComponents['Blurb'] = Component14;
+import Component14 from '../src/atoms/story-elements/blockquote/blockquote.tsx';
+reactComponents['BlockQuote'] = Component14;
 
-import Component15 from '../src/atoms/story-elements/daily-motion-element/daily-motion-element.tsx';
-reactComponents['DailyMotionElement'] = Component15;
+import Component15 from '../src/atoms/story-elements/blurb/blurb.tsx';
+reactComponents['Blurb'] = Component15;
 
-import Component16 from '../src/atoms/story-elements/embed/embed.tsx';
-reactComponents['Embed'] = Component16;
+import Component16 from '../src/atoms/story-elements/daily-motion-element/daily-motion-element.tsx';
+reactComponents['DailyMotionElement'] = Component16;
 
-import Component17 from '../src/atoms/story-elements/attachment/attachment.tsx';
-reactComponents['FacebookElement'] = Component17;
+import Component17 from '../src/atoms/story-elements/embed/embed.tsx';
+reactComponents['Embed'] = Component17;
 
-import Component18 from '../src/atoms/story-elements/facebook-element/facebook-element.tsx';
+import Component18 from '../src/atoms/story-elements/attachment/attachment.tsx';
 reactComponents['FacebookElement'] = Component18;
 
-import Component19 from '../src/molecules/header-card/header-card.tsx';
-reactComponents['HeaderCard'] = Component19;
+import Component19 from '../src/atoms/story-elements/facebook-element/facebook-element.tsx';
+reactComponents['FacebookElement'] = Component19;
 
-import Component20 from '../src/atoms/story-elements/image-element/image-element.tsx';
-reactComponents['ImageElement'] = Component20;
+import Component20 from '../src/molecules/header-card/header-card.tsx';
+reactComponents['HeaderCard'] = Component20;
 
-import Component21 from '../src/atoms/story-elements/image-gallery-element/image-gallery-element.tsx';
-reactComponents['ImageGalleryElement'] = Component21;
+import Component21 from '../src/atoms/story-elements/image-element/image-element.tsx';
+reactComponents['ImageElement'] = Component21;
 
-import Component22 from '../src/atoms/infinite-scroll/infinite-scroll.tsx';
-reactComponents['InfiniteScroll'] = Component22;
+import Component22 from '../src/atoms/story-elements/image-gallery-element/image-gallery-element.tsx';
+reactComponents['ImageGalleryElement'] = Component22;
 
-import Component23 from '../src/atoms/story-elements/instagram-element/instagram-element.tsx';
-reactComponents['InstagramElement'] = Component23;
+import Component23 from '../src/atoms/infinite-scroll/infinite-scroll.tsx';
+reactComponents['InfiniteScroll'] = Component23;
 
-import Component24 from '../src/atoms/story-elements/text/q-and-a/q-and-a.tsx';
-reactComponents['QAndA'] = Component24;
+import Component24 from '../src/atoms/story-elements/instagram-element/instagram-element.tsx';
+reactComponents['InstagramElement'] = Component24;
 
-import Component25 from '../src/atoms/story-elements/text/question/question.tsx';
-reactComponents['Question'] = Component25;
+import Component25 from '../src/atoms/story-elements/text/q-and-a/q-and-a.tsx';
+reactComponents['QAndA'] = Component25;
 
-import Component26 from '../src/atoms/story-elements/text/summary/summary.tsx';
-reactComponents['Summary'] = Component26;
+import Component26 from '../src/atoms/story-elements/text/question/question.tsx';
+reactComponents['Question'] = Component26;
 
-import Component27 from '../src/atoms/story-elements/table-element/table-element.tsx';
-reactComponents['TableElement'] = Component27;
+import Component27 from '../src/atoms/story-elements/text/summary/summary.tsx';
+reactComponents['Summary'] = Component27;
 
-import Component28 from '../src/atoms/story-elements/text/text.tsx';
-reactComponents['Text'] = Component28;
+import Component28 from '../src/atoms/story-elements/table-element/table-element.tsx';
+reactComponents['TableElement'] = Component28;
 
-import Component29 from '../src/atoms/story-elements/title/title.tsx';
-reactComponents['Title'] = Component29;
+import Component29 from '../src/atoms/story-elements/text/text.tsx';
+reactComponents['Text'] = Component29;
 
-import Component30 from '../src/atoms/story-elements/unsupported/unsupported.tsx';
+import Component30 from '../src/atoms/story-elements/title/title.tsx';
 reactComponents['Title'] = Component30;
 
-import Component31 from '../src/atoms/story-elements/twitter-element/twitter-element.tsx';
-reactComponents['TwitterElement'] = Component31;
+import Component31 from '../src/atoms/story-elements/unsupported/unsupported.tsx';
+reactComponents['Title'] = Component31;
 
-import Component32 from '../src/atoms/story-elements/vidible-element/vidible-element.tsx';
-reactComponents['VidibleElement'] = Component32;
+import Component32 from '../src/atoms/story-elements/twitter-element/twitter-element.tsx';
+reactComponents['TwitterElement'] = Component32;
 
-import Component33 from '../src/atoms/story-elements/youtube/youtube.tsx';
-reactComponents['YouTube'] = Component33;
+import Component33 from '../src/atoms/story-elements/vidible-element/vidible-element.tsx';
+reactComponents['VidibleElement'] = Component33;
 
-import Component34 from '../src/molecules/navbar/navbar.tsx';
-reactComponents['Navbar'] = Component34;
+import Component34 from '../src/atoms/story-elements/youtube/youtube.tsx';
+reactComponents['YouTube'] = Component34;
 
-import Component35 from '../src/molecules/related-stories/related-stories.tsx';
-reactComponents['RelatedStories'] = Component35;
+import Component35 from '../src/molecules/navbar/navbar.tsx';
+reactComponents['Navbar'] = Component35;
 
-import Component36 from '../src/atoms/video-web-story/video-web-story.tsx';
-reactComponents['VideoWebStory'] = Component36;
+import Component36 from '../src/molecules/related-stories/related-stories.tsx';
+reactComponents['RelatedStories'] = Component36;
 
-import Component37 from '../src/templates/visual-story/visual-story.tsx';
-reactComponents['VisualStory'] = Component37;
+import Component37 from '../src/atoms/video-web-story/video-web-story.tsx';
+reactComponents['VideoWebStory'] = Component37;
 
-import Component38 from '../src/molecules/webengage/webengage.tsx';
-reactComponents['WebEngage'] = Component38;
+import Component38 from '../src/templates/visual-story/visual-story.tsx';
+reactComponents['VisualStory'] = Component38;
+
+import Component39 from '../src/molecules/webengage/webengage.tsx';
+reactComponents['WebEngage'] = Component39;
