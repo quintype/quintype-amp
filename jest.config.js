86ea209fc128268e4b13c905742002efa19b84a1
Navya <navya@quintypes-MacBook-Air.local>
2025-10-08 16:56:20 +0530

    react upgrade done, enzyme needs to be checked

--- jest.config.js
+++ jest.config.js
@@ -4,26 +4,9 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest",
     "^.+\\.js$": "babel-jest" // added this
   },
-  setupFiles: ["<rootDir>/jest-setup.js"],
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   snapshotSerializers: ["enzyme-to-json/serializer"],
-  testTimeout: 90000,
-  testEnvironment: "jsdom",
-  moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-    "^cheerio$": "<rootDir>/node_modules/cheerio/dist/commonjs/index.js" //mapping cheerio to use its commonjs version
-  },
-  transformIgnorePatterns: [
-    "node_modules/(?!(cheerio|parse5|domhandler|domutils|css-select|nth-check|boolbase|enzyme|enzyme-to-json|enzyme-adapter-react-18|@cfaester|undici)/)" //include the necessary packages
-  ],
-  extensionsToTreatAsEsm: [".ts", ".tsx"],
-  globals: {
-    "ts-jest": {
-      useESM: true
-    },
-    TextEncoder: require("util").TextEncoder,
-    TextDecoder: require("util").TextDecoder
-  }
+  testTimeout: 90000
 };
