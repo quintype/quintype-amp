91e3db65c30bdb86a72e0982a75fa45f87c8902e
VeenaYemmiganur <veena.yemmiganur@quintype.com>
2025-12-10 21:08:28 +0530

    add. amp-widget initial commit

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const BODY_AD_MAX_LIMIT = 6;
+export const BODY_WIDGET_MAX_LIMIT = 6;
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,3 +3,22 @@ export function getVideoElement(card) {
     (el) => el.type === "jsembed" || (el.type === "video" && el.subtype === "video-clip")
   );
 }
+
+export const decodeB64 = (b64Code) => {
+  try {
+    return decodeURI(Buffer.from(b64Code, "base64").toString());
+  } catch (err) {
+    console.error("Failed to decode base 64 code ", err);
+    return null;
+  }
+};
+
+export const isBase64 = (str) => {
+  if (!str || typeof str !== "string") return false;
+
+  try {
+    return btoa(atob(str)) === str;
+  } catch (e) {
+    return false;
+  }
+};
