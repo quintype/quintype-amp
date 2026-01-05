f7b34e7431165aba21dabb6a5430d10ef8a818dc
VeenaYemmiganur <104062026+VeenaYemmiganur@users.noreply.github.com>
2026-01-05 11:43:32 +0530

    Amp custom widget task (#502)
    * add. up to 6 widgets enabled on amp story template - amp-custom-widget commit

--- src/molecules/full-story-content/full-story-content.tsx
+++ src/molecules/full-story-content/full-story-content.tsx
@@ -1,12 +1,14 @@
 // This file should not exist. Move all components inside appropriate atoms/molecules
 
 import React from "react";
-import { StoryElement } from "../../atoms";
+import { Spacer, StoryElement } from "../../atoms";
 import { Fragment } from "react";
 import { BodyAd } from "../ads";
 import { StoryPageSlots } from "../slots";
 import get from "lodash.get";
-import { BODY_AD_MAX_LIMIT } from "../../constants";
+import { BODY_AD_MAX_LIMIT, BODY_WIDGET_MAX_LIMIT } from "../../constants";
+import { decodeB64, isBase64 } from "../../utils/utils";
+import { AdWrapper } from "../ads/shared/components";
 
 const { DefaultStoryCardSlot } = StoryPageSlots;
 
@@ -18,6 +20,16 @@ export const FullStoryContent = ({ story, config }) => {
   const hasMultipleBodyAds = Array.isArray(bodyAdsConfig) && bodyAdsConfig.length > 0;
   const bodyAdsToRender = (bodyAdsConfig || []).slice(0, BODY_AD_MAX_LIMIT);
 
+  const customAmpWidgets = get(config, ["opts", "customAmpWidgets"]);
+  const widgets =
+    get(config, ["additionalConfig", "story", "ampWidgets"]) ||
+    (typeof customAmpWidgets === "function" ? customAmpWidgets() : customAmpWidgets);
+
+  const widgetArray = Array.isArray(widgets) ? widgets : [];
+
+  const bodyWidgetsToRender = widgetArray.slice(0, BODY_WIDGET_MAX_LIMIT);
+  const enabledWidgets = bodyWidgetsToRender.filter((w) => w?.enable && w?.code);
+
   return story.cards.map((card, cardIdx) => {
     const customCardRender = get(config, ["opts", "render", "storyCardRenderProps"], {});
     const cardtype = get(card, ["metadata", "attributes", "cardtype", "0"], "");
@@ -25,6 +37,12 @@ export const FullStoryContent = ({ story, config }) => {
       return customCardRender[cardtype]({ story, config, card, counter });
     }
     const adSlot = hasMultipleBodyAds ? bodyAdsToRender[cardIdx] : undefined;
+
+    const slot = bodyWidgetsToRender[cardIdx];
+
+    const widgetSlot =
+      cardIdx < BODY_WIDGET_MAX_LIMIT && slot?.enable ? (isBase64(slot.code) ? decodeB64(slot.code) : slot.code) : null;
+
     return (
       <Fragment key={card.id}>
         {card["story-elements"].map((element, storyElementIdx) => (
@@ -36,9 +54,21 @@ export const FullStoryContent = ({ story, config }) => {
             counter={counter}
           />
         ))}
-        {hasMultipleBodyAds
-          ? adSlot && <BodyAd adSlot={adSlot} templateName="default" />
-          : cardIdx === 0 && <BodyAd templateName="default" />}
+        {enabledWidgets.length > 0 ? (
+          <>
+            {cardIdx === 0 && <BodyAd templateName="default" />}
+            {widgetSlot && (
+              <AdWrapper>
+                <div dangerouslySetInnerHTML={{ __html: widgetSlot }} />
+                <Spacer token="s" />
+              </AdWrapper>
+            )}
+          </>
+        ) : hasMultipleBodyAds ? (
+          adSlot && <BodyAd adSlot={adSlot} templateName="default" />
+        ) : (
+          cardIdx === 0 && <BodyAd templateName="default" />
+        )}
         <DefaultStoryCardSlot index={cardIdx} card={card} />
       </Fragment>
     );
